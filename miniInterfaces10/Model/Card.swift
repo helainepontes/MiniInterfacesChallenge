137711b02f159c94a6838897f37a4f9848290782
Lucas Alves <tilucasoli@gmail.com>
2020-04-28 12:18:26 -0300

    Agora DEUUUUUUUU UHUUULLLLLLL

---
+++ miniInterfaces10/Model/Card.swift
@@ -0,0 +1,25 @@
+//
+//  Card.swift
+//  miniInterfaces10
+//
+//  Created by Lucas Oliveira on 28/04/20.
+//  Copyright © 2020 Helaine Pontes. All rights reserved.
+//
+
+import Foundation
+
+class Card {
+    let photo: String
+    let feelingColor: FeelingCard
+    let title: String
+    let description: String
+    
+    init(photo: String, feelingColor: FeelingCard, title: String, description: String) {
+        self.photo = photo
+        self.feelingColor = feelingColor
+        self.title = title
+        self.description = description
+        let a = 10
+        
+    }
+}
